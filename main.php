6298133b8f8a1a2d7eb3ce08e66a6fa630a56bc8
Oaxz <85687521+0axz-tools@users.noreply.github.com>
2022-04-06 11:52:33 +0800

    Create main.php

---
+++ main.php
@@ -0,0 +1,46 @@
+<?php
+$nama_file = readline("Masukan file : ");
+$file = fopen("valid.txt","w");  
+$file2 = fopen("not valid.txt","w"); 
+// set API Access Key
+$access_key = 'c3f026b17a4b0238eebbbc3499f59475';
+// set phone number
+//$phone_number = '14158586273';
+if (file_exists($nama_file)) {
+    $fh = fopen($nama_file, "r");
+    while(!feof($fh)){
+        $line = fgets($fh);
+        if($line === null)break;
+        $string = trim(preg_replace('/\s\s+/', ' ', $line));
+        $sate = '61'. $string;
+        // Initialize CURL:
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://apilayer.net/api/validate?access_key='. $access_key .'&number='. $sate); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch). PHP_EOL;
+     //  var_dump(json_decode($output, true)); 
+        $validationResult = json_decode($output, true);      
+        if ($validationResult['valid']) {
+            echo ' Phone number is valid => '.$sate.'|'. $validationResult["carrier"] . PHP_EOL;
+           // echo $sate. " Phone number is valid". PHP_EOL;
+                 $hasil = ' Phone number is valid => '.$sate.'|'. $validationResult["carrier"] . PHP_EOL;
+                 fwrite($file,$hasil);               
+                 
+        } 
+        
+        else {
+            echo $sate. " Phone number is not valid". PHP_EOL;
+                
+                 
+        }
+        
+    }
+   
+        
+
+
+fclose($fh); 
+fclose($file);
+}else {
+    echo "File tidak di temukan";
+}
